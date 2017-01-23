ea86fb4ef7c4c86d908807b8a7f6126b37b120fb
Attila_Borbas <attila_borbas@epam.com>
2017-01-23 16:54:48 +0100

    Add Mock

--- CandyShop/src/test/java/com/epam/m6w1/TestOrder.java
+++ CandyShop/src/test/java/com/epam/m6w1/TestOrder.java
@@ -1,13 +1,16 @@
 package com.epam.m6w1;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.mockito.Mockito;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import com.epam.m6w1.Choclate;
 
+/*
 import static com.epam.m6w1.Candy.Taste.CLASSIC;
 import static com.epam.m6w1.Candy.Taste.HONEY;
 import static com.epam.m6w1.Candy.Taste.MENTHOL;
-
+*/
 public class TestOrder {
 
     private Order order;
@@ -24,7 +27,8 @@ public class TestOrder {
         System.out.println("After Method");
         order.clear();
     }
-
+    /*
+    @Ignore
     @Test
     public void testAddItemTotalQuantaty() {
         Choclate choclate = new Choclate();
@@ -33,23 +37,34 @@ public class TestOrder {
         Assert.assertEquals(totalQuantity, 2);
     }
 
+    @Ignore
     @Test
     public void testAddItemTotalPrice() {
         Choclate choclate = new Choclate();
+        //Taste taste = choclate.getSpecialParameter();
         order.addItem(choclate, 2);
         int totalPrice = order.getTotalPrice();
         Assert.assertEquals(totalPrice, 420);
     }
 
-    @Test(dataProvider = "itemFeeder")
-    public void testCreateCandy(String name, int quantaty, int price, Candy.Taste taste) {
-        Candy candy = new Candy("Cukor", 1, 200, MENTHOL);
+    @Ignore
+    @Test(dataProvider = "candyFeeder")
+    public void testCreatingCandy(String name, int quantaty, int price, Candy.Taste taste) {
+        Candy candy = new Candy(name, quantaty, price, taste);
 
     }
 
-    @DataProvider (name = "itemFeeder")
+    @DataProvider (name = "candyFeeder")
     public Object [][] fedder() {
-        return new Object [][] {{"Cukor1", 1, 100, MENTHOL}, {"Cukor2", 2, 200, HONEY}, {"Cukor3", 3, 300, CLASSIC}};
+        return new Object [][] {{"Candy1", 1, 100, MENTHOL}, {"Candy2", 2, 200, HONEY}, {"Candy3", 3, 300, CLASSIC}};
+    }
+    */
+    @Test
+    public void testMockGetTotalPrice() {
+        Order orderForMock;
+        orderForMock = Mockito.mock(Order.class);
+        Mockito.when(orderForMock.getTotalPrice()).thenReturn(99);
+        Assert.assertEquals(50, orderForMock.getTotalPrice());
     }
 
 
